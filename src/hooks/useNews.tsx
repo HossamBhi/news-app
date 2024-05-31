410070e82f15a683dfbffef8c019a2a66509ed1e
Hossam Bhi Pasha <hossambasha9900@gmail.com>
2024-05-31 21:21:10 +0300

    Remove Dummy Data and change API Key

--- src/hooks/useNews.tsx
+++ src/hooks/useNews.tsx
@@ -2,25 +2,24 @@ import {useQuery} from '@tanstack/react-query';
 import axios, {AxiosError} from 'axios';
 import Config from 'react-native-config';
 import {NewsType} from '../utils/types';
-import {newsData} from '../db';
-
+// import {newsData} from '../db';
 const useNews = () => {
   return useQuery<{articles: NewsType[]}, Error>({
     queryKey: ['news'],
     queryFn: () =>
-      new Promise((resolve, reject) =>
-        setTimeout(() => {
-          resolve(newsData);
-        }, 1000),
-      ),
-    // fetchNews(),
+      // new Promise((resolve, reject) =>
+      //   setTimeout(() => {
+      //     resolve(newsData);
+      //   }, 1000),
+      // ),
+      fetchNews(),
   });
 };
 
 const fetchNews = async () => {
   try {
     const {data} = await axios.get(
-      `/everything?q=tesla&from=2024-04-29&sortBy=publishedAt&apiKey=${Config.API_KEY}`,
+      `/everything?q=tesla&sortBy=publishedAt&apiKey=${Config.API_KEY}`,
     );
     return data;
   } catch (error) {
